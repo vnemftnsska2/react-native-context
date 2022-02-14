c3442882e6b2770fa2cb543ec5f6732fdd94001b
tnsska2 <vnemftnsska2@naver.com>
2022-02-14 23:48:52 +0900

    Add: Stack Style

--- src/navigations/Stack.js
+++ src/navigations/Stack.js
@@ -1,5 +1,8 @@
 import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { Platform } from "react-native";
+//Components
 import Home from "../screens/Home";
 import List from "../screens/List";
 import Item from "../screens/Item";
@@ -20,13 +23,36 @@ const StackNavigation = () => {
         },
         headerTitleStyle: { color: "#ffffff", fontSize: 24 },
         headerTitleAlign: "center",
+        headerTitle: ({ style }) => (
+          <MaterialCommunityIcons name="react" style={style} />
+        ),
       }}
     >
       <Stack.Screen name="Home" component={Home} />
       <Stack.Screen
         name="List"
         component={List}
-        options={{ headerTitle: "List Screen" }}
+        options={{
+          headerTitle: "List Screen",
+          headerBackTitleVisible: true,
+          headerBackTitle: "Prev",
+          headerTitleStyle: { fontSize: 24 },
+          headerTintColor: "#e74c3c",
+          headerBackImage: ({ tintColor }) => {
+            const style = {
+              marginRight: 5,
+              marginLeft: Platform.OS === "iso" ? 11 : 0,
+            };
+            return (
+              <MaterialCommunityIcons
+                name="keyboard-backspace"
+                size={30}
+                color={tintColor}
+                style={style}
+              />
+            );
+          },
+        }}
       />
       <Stack.Screen name="Detail" component={Item} />
     </Stack.Navigator>
--- src/screens/Item.js
+++ src/screens/Item.js
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useLayoutEffect } from "react";
 import styled from "styled-components/native";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 const Container = styled.View`
   flex: 1;
@@ -12,7 +13,33 @@ const StyledText = styled.Text`
   margin-bottom: 10px;
 `;
 
-const Item = ({ route }) => {
+const Item = ({ navigation, route }) => {
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerBackTitleVisible: false,
+      headerTintColor: "#ffffff",
+      headerLeft: ({ onPress, tintColor }) => {
+        return (
+          <MaterialCommunityIcons
+            name="keyboard-backspace"
+            size={30}
+            style={{ marginLeft: 11 }}
+            color={tintColor}
+            onPress={onPress}
+          />
+        );
+      },
+      headerRight: ({ tintColor }) => (
+        <MaterialCommunityIcons
+          name="home-variant"
+          size={30}
+          style={{ marginRight: 11 }}
+          color={tintColor}
+          onPress={() => navigation.popToTop()}
+        />
+      ),
+    });
+  }, []);
   return (
     <Container>
       <StyledText>Item</StyledText>
